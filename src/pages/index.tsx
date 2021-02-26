08e88875e94444543bd5b18ce315669d95dec22b
Uéslei Suptitz <ueslei.suptitz@paipe.co>
2021-02-26 16:28:46 -0300

    mobile icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,6 +28,7 @@ export default function Home({
     >
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
         <title>Move.it</title>
       </Head>
       <div className={styles.container}>
